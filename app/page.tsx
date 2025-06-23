d306c778ad164799da1c56f337e939cf1812f27c
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-06-23 00:52:14 -0300

    feat: add agendamentos component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Header from "./_components/Header";
 import { Button } from "./_components/ui/button";
 import { Input } from "./_components/ui/input";
 import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar, AvatarImage } from "./_components/ui/avatar";
 
 export default function Home() {
   return (
@@ -12,6 +15,7 @@ export default function Home() {
         <h2 className="text-xl font-bold">Olá, João</h2>
         <p>Sexta-feira, 20 de Junho</p>
 
+        {/* Search */}
         <div className="flex gap-2 mt-6 items-center">
           <Input placeholder="Pesquisar..." />
           <Button>
@@ -19,6 +23,7 @@ export default function Home() {
           </Button>
         </div>
 
+        {/* Banner */}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             fill
@@ -27,6 +32,29 @@ export default function Home() {
             className="object-cover rounded-xl"
           />
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="w-6 h-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"></AvatarImage>
+                </Avatar>
+                <p className="text-sm">Barbearia do João</p>
+              </div>
+            </div>
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center px-5 border-l border-solid">
+              <p className="text-sm">Junho</p>
+              <p className="text-2xl">23</p>
+              <p className="text-sm">10:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
