ec2a02b835bd223efc136250eb3fb76bbcf4ab15
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-06-25 16:57:33 -0300

    feat: ensure services wil show in barbershop page

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,3 +1,4 @@
+import ServiceItem from "@/app/_components/ServiceItem";
 import { Button } from "@/app/_components/ui/button";
 import { db } from "@/app/_lib/prisma";
 import { ChevronLeft, MapPin, MenuIcon, StarIcon } from "lucide-react";
@@ -15,6 +16,9 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
   const barbershop = await db.barbershop.findUnique({
     where: {
       id: params.id,
+    },
+    include: {
+      services: true,
     },
   });
 
@@ -54,6 +58,7 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
         </Button>
       </div>
 
+      {/* Endereço */}
       <div className="border-b border-solid p-5">
         <h1 className="text-xl mb-3 font-bold">{barbershop.name}</h1>
         <div className="flex items-center gap-2">
@@ -70,6 +75,16 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       <div className="p-5 space-y-2 border-b border-solid">
         <h2 className="text-xs font-bold text-gray-400 uppercase">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop.description}</p>
+      </div>
+
+      {/* Serviços */}
+      <div className="p-5 space-y-3">
+        <h2 className="text-gray-400 text-xs font-bold uppercase">Serviços</h2>
+        <div className="space-y-3">
+          {barbershop.services.map((service) => (
+            <ServiceItem key={service.id} service={service} />
+          ))}
+        </div>
       </div>
     </div>
   );
