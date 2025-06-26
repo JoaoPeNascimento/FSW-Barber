e248febd27a8b10c0d3767920b87c4cc3665393e
JoaoPeNascimento <joao.vitorpe.1010@gmail.com>
2025-06-26 18:56:11 -0300

    feat: add login with google button

--- app/_components/SidebarContent.tsx
+++ app/_components/SidebarContent.tsx
@@ -1,10 +1,17 @@
-import { Calendar1Icon, HomeIcon, LogOutIcon } from "lucide-react";
+import { Calendar1Icon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 import { quickSearchOptions } from "../_constants/search";
-import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Image from "next/image";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 const SidebarContent = () => {
   return (
@@ -14,14 +21,37 @@ const SidebarContent = () => {
       </SheetHeader>
 
       {/* Avatar */}
-      <div className="flex items-center border-b border-solid py-5 space-x-3">
-        <Avatar>
+      <div className="flex items-center border-b border-solid py-5 space-x-3 justify-between">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size={"icon"}>
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça login na plataforma!</DialogTitle>
+              <DialogDescription>
+                Conecte-se utilizando sua conta Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant={"outline"} className="font-bold">
+              <Image src="/google.svg" alt="Google" width={18} height={18} />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/* <Image src="/google.svg" alt="Google" width={18} height={18} /> */}
+
+        {/* <Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Avatar>
         <div>
           <p className="font-bold">João Nascimento</p>
           <p className="text-sm">joao@joao.com</p>
-        </div>
+        </div> */}
       </div>
 
       {/* Botões iniciais */}
